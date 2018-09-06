90c44f2b03d072cc1dbb42925b7fab3943aaa018
catatonic2aucourant <mohsinjackspiro@gmail.com>
2018-09-06 23:43:04 +0530

    Made code more readable

--- text_only_model.py
+++ text_only_model.py
@@ -20,38 +20,39 @@ MODEL_IDENTIFIER = "linear_model_20k"
 def main():
     train = pd.read_csv(config.CLEAN_TRAIN_DATA)
     test = pd.read_csv(config.CLEAN_TEST_DATA)
-
     print("Shape of train and test", train.shape, test.shape)
-    print("Columns in train are", train.columns)
+    print("Columns in train are", list(train.columns))
 
+    # Map labels to integers
     train['target'] = train.Tag.map(config.TAG_DICT)
 
+    # Replicate train/test split strategy for cross validation
     train["target_str"] = train["Domain"].astype(str) + train["Tag"].astype(str)
     cvlist = list(GroupKFold(5).split(train, groups=train["target_str"].astype('category')))
 
+    # TFIDF vectorizers for words and characters of URL.
+    # Parameters were chosen randomly due to lack of time. Tuning these could improve score
     vec1 = TfidfVectorizer(ngram_range=(1, 4), analyzer="char",
                            min_df=1000, max_df=1.0, strip_accents='unicode', use_idf=1,
                            smooth_idf=1, sublinear_tf=1, max_features=20000)
     vec2 = TfidfVectorizer(ngram_range=(1, 1), analyzer="word",
                            min_df=1000, max_df=1.0, strip_accents='unicode', use_idf=1,
                            smooth_idf=1, sublinear_tf=1, max_features=20000)
-
-    vec = FeatureUnion(
-        [("char", vec1), ("word", vec2)])
-
+    vec = FeatureUnion([("char", vec1), ("word", vec2)])
     vec.fit(pd.concat([train["text"], test["text"]]))
     X = vec.transform(train["text"])
     X_test = vec.transform(test["text"])
-
     print("Shape of train and test after TFIDF transformation".format( X.shape, X_test.shape))
 
+    # Run Logistic regression in cross validation to get out-of-fold predictions on validation
+    # Fit again on all train data and predict on test data
     y = train["target"].values
-    model = LogisticRegression(C=1, class_weight='balanced', dual=True, solver='liblinear', max_iter=100, verbose=1)
-    y_preds = cross_val_predict(model, X, y, cv=cvlist, n_jobs=2, method='predict_proba', verbose=10)
+    model = LogisticRegression(C=1, class_weight='balanced', dual=True, solver='liblinear')
+    y_preds = cross_val_predict(model, X, y, cv=cvlist, method='predict_proba')
     y_test_preds = model.fit(X, y).predict_proba(X_test)
-    gc.collect()
     print("Overall f1 score for text only model", f1_score(y, np.argmax(y_preds, axis=1), average="weighted"))
 
+    # Save out-of-fold and test predictions
     oof_preds: pd.DataFrame = train[['Webpage_id']]
     for i, col in enumerate(config.LABEL_COLS):
         oof_preds.loc[:, col] = y_preds[:, i]
